3126cf0e1ed1917788379f7f47bb215c22f1e060
Hatim Sawai <hatimsawai76@gmail.com>
2023-09-10 12:39:51 +0530

    MERN Stack Initiate
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='text-2xl text-center mt-10 font-bold'>
+      App
+    </div>
+  )
+}
+
+export default App
