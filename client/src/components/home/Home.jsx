e5f4772e46a1db58f693499e0cf4a7f0e2e49d06
YS028 <ythink28@gmail.com>
2023-09-18 22:19:06 +0530

    update home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import Hero from './Hero'
-import Function from './Function'
-import Lawyers from './Lawyers'
-import Page from './Page'
-import ProBono from './ProBono'
 
 const Home = () => {
   return (
-    <div className="mt-6">
-      <Hero/>
-      <Function/>
-      <Lawyers/>
-      <Page/>
-      <ProBono/>
+    <div>
+      hello
     </div>
   )
 }
